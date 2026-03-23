0ce27e22ff757c5bf7ef6f00f1ba80f9ec443519
noory3min-cloud <noory3min@gmail.com>
2026-03-23 19:49:17 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,13 @@
+A = int(input())
+B = int(input())
+
+if(A>0):
+    if(B>0):
+        print(1)
+    elif(B<0):
+        print(4)
+elif(A<0):
+    if(B>0):
+        print(2)
+    elif(B<0):
+        print(3)
