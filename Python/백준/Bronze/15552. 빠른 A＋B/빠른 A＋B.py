6bfd0708c0e618b3d05d41f3d5c9c4fa2ca87a04
noory3min-cloud <noory3min@gmail.com>
2026-03-24 17:44:49 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1280 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ Python/백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -1,11 +1,7 @@
 import sys
 
 T = int(sys.stdin.readline())
-A, B = [0]*T, [0]*T
 
 for i in range(T):
-    A[i], B[i] = map(int, sys.stdin.readline().split())
-    
-for i in range(T):
-    print(A[i] + B[i])
-    
+    a, b = map(int, sys.stdin.readline().split())
+    print(a + b)
