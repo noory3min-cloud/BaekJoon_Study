04f7907a01559673a995ab787b33850f19458a77
noory3min-cloud <noory3min@gmail.com>
2026-03-25 10:40:42 +0900

    [Bronze V] Title: X보다 작은 수, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+N, X = map(int, input().split())
+A = [0]*N
+
+A = list(map(int, input().split()))
+
+for i in range(N):
+    if A[i] < X:
+        print(A[i], end=' ')
