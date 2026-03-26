1df6dc0b17e77d84be7a08b2c9a78044cf3e826b
noory3min-cloud <noory3min@gmail.com>
2026-03-26 23:35:19 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for i in range(T):
+    R, S = input().split()
+    for j in range(len(S)):
+        print(S[j]*int(R), end="")
+    print()
